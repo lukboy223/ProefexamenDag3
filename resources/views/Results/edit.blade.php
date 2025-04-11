33095dffb67cd494e998ddff6e47af7bb746cbdf
lukboy223 <ltvanekeren@gmail.com>
2025-04-11 14:08:51 +0200

    visual verbetering

--- resources/views/Results/edit.blade.php
+++ resources/views/Results/edit.blade.php
@@ -4,7 +4,7 @@
     {{-- title on the top of the screen --}}
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            score aanpassen
+            Score aanpassen van {{ $result->FullName }}
         </h2>
     </x-slot>
 
